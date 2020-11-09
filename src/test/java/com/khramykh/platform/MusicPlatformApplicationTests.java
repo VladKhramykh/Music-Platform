5c553525ea5ddb2d9479adf95351a2c917e1c922
Vlad Khramykh <vlad.kramykh@wedextim.com>
2020-11-09 15:40:42 +0300

    initial commit

---
+++ src/test/java/com/khramykh/platform/MusicPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khramykh.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
