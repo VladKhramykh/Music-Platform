aa75bcffbad5daeec32984bd1f0eac9ea15e33e8
Vlad Khramykh <vlad.kramykh@wedextim.com>
2020-11-18 20:00:03 +0300

    added: Album Create/Update commands

---
+++ src/main/java/com/khramykh/platform/application/albumsApi/commands/AlbumUpdateCommand.java
@@ -0,0 +1,24 @@
+package com.khramykh.platform.application.albumsApi.commands;
+
+import com.khramykh.platform.domain.commons.enums.AlbumTypes;
+import com.khramykh.platform.domain.entities.Artist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlbumUpdateCommand {
+    private int id;
+    private String name;
+    private String description;
+    private AlbumTypes type;
+    private String photoUri;
+    private Date releaseDate;
+    private Set<Artist> artists = new HashSet<>();
+}
